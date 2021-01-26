9c8558e739a2a63105b7fd4be82d02a05bb75f24
Alex Covington <acovingt@amd.com>
2021-01-26 15:42:55 -0800

    Adding contention adjustements in PortableThreadPool

--- src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
@@ -20,6 +20,8 @@ namespace System.Threading
                     AppContextConfigHelper.GetInt32Config("System.Threading.ThreadPool.UnfairSemaphoreSpinLimit", 70, false),
                     onWait: () =>
                     {
+                        Contention.ThreadPoolContention.ReportWait();
+
                         if (PortableThreadPoolEventSource.Log.IsEnabled())
                         {
                             PortableThreadPoolEventSource.Log.ThreadPoolWorkerThreadWait(
@@ -53,6 +55,7 @@ namespace System.Threading
                         while (TakeActiveRequest(threadPoolInstance))
                         {
                             Volatile.Write(ref threadPoolInstance._separated.lastDequeueTime, Environment.TickCount);
+                            Contention.ThreadPoolContention.ReportWork();
                             if (!ThreadPoolWorkQueue.Dispatch())
                             {
                                 // ShouldStopProcessingWorkNow() caused the thread to stop processing work, and it would have
@@ -118,6 +121,12 @@ namespace System.Threading
                             newCounts.SubtractNumExistingThreads(1);
                             short newNumExistingThreads = (short)(numExistingThreads - 1);
                             short newNumThreadsGoal = Math.Max(threadPoolInstance._minThreads, Math.Min(newNumExistingThreads, newCounts.NumThreadsGoal));
+
+                            if (Contention.ThreadPoolContention.ContentionDetected)
+                            {
+                                newNumThreadsGoal = Math.Max(threadPoolInstance._minThreads, (short)(numExistingThreads - Contention.ThreadPoolContention.StepDown));
+                            }
+
                             newCounts.NumThreadsGoal = newNumThreadsGoal;
 
                             ThreadCounts oldCounts = threadPoolInstance._separated.counts.InterlockedCompareExchange(newCounts, counts);
